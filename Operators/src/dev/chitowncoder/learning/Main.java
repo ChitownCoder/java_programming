52c38f966ec06adf2841176642f5faa1b852c229
ChitownCoder <chitowncoder@gmail.com>
2022-02-04 18:20:13 -0600

    Completed an Operator Challenge

--- Operators/src/dev/chitowncoder/learning/Main.java
+++ Operators/src/dev/chitowncoder/learning/Main.java
@@ -76,6 +76,21 @@ public class Main {
         boolean wasCar = isCar ? true : false;
         if (wasCar) {
             System.out.println("wasCar is true");
+
+//            CHALLENGE BELOW
+
+
+            double myFirstValue = 20.00d;
+            double mySecondValue = 80.00d;
+            double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+            System.out.println("MyValuesTotal = " + myValuesTotal);
+            double theRemainder = myValuesTotal % 40.00d;
+            System.out.println("theRemainder = " + theRemainder);
+            boolean isNoRemainder = (theRemainder == 0) ? true : false;
+            System.out.println("isNoRemainder = " + isNoRemainder);
+            if (!isNoRemainder) {
+                System.out.println("got some remainder.");
+            }
         }
     }
 }
